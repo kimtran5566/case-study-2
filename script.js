760e0a666fa919894af038daa1c849de72b0ee7b
kimtran5566 <lktran5566@gmail.com>
2023-12-01 18:41:41 +0700

    new update

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".carousel");
-    let currentIndex = 0;
-  
-    function showSlide(index) {
-      const totalSlides = document.querySelectorAll(".slide").length;
-      if (index < 0) {
-        currentIndex = totalSlides - 1;
-      } else if (index >= totalSlides) {
-        currentIndex = 0;
-      } else {
-        currentIndex = index;
-      }
-  
-      const translateValue = -currentIndex * 100 + "%";
-      carousel.style.transform = `translateX(${translateValue})`;
-    }
-  
-    // Auto slide change interval (adjust as needed)
-    setInterval(function () {
+  const carousel = document.querySelector(".carousel");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  let currentIndex = 0;
+
+  nextBtn.addEventListener("click", function () {
+    if (currentIndex < carousel.children.length - 1) {
       currentIndex++;
-      showSlide(currentIndex);
-    }, 5000); // Change slide every 5 seconds (adjust as needed)
-  
-    // Optionally, you can add event listeners for navigation buttons or gestures
-    // For example, left and right arrow keys for navigation
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "ArrowLeft") {
-        currentIndex--;
-        showSlide(currentIndex);
-      } else if (event.key === "ArrowRight") {
-        currentIndex++;
-        showSlide(currentIndex);
-      }
-    });
+    } else {
+      currentIndex = 0;
+    }
+    updateCarousel();
   });
-  
+
+  prevBtn.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = carousel.children.length - 1;
+    }
+    updateCarousel();
+  });
+
+  function updateCarousel() {
+    const translateValue = -currentIndex * 100 + "%";
+    carousel.style.transform = `translateX(${translateValue})`;
+  }
+});
